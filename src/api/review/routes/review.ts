acc5e76e538d4be4456a01cc868af3c782805142
subhamnaththapa <subhamnaththapa@github.com>
2025-12-03 11:35:39 -0500

    Enable public access to review endpoints

--- src/api/review/routes/review.ts
+++ src/api/review/routes/review.ts
@@ -4,4 +4,14 @@
 
 import { factories } from "@strapi/strapi";
 
-export default factories.createCoreRouter("api::review.review");
+export default factories.createCoreRouter("api::review.review", {
+  only: ["find", "findOne"],
+  config: {
+    find: {
+      auth: false,
+    },
+    findOne: {
+      auth: false,
+    },
+  },
+});
